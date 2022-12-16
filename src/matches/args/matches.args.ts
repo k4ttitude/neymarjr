f698e12b4e066119595bcc89c0056de0ea272023
Hieu <pmh.ars14@gmail.com>
2022-12-16 22:53:56 +0700

    ValuesOf type

--- src/matches/args/matches.args.ts
+++ src/matches/args/matches.args.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { ValuesOf } from '../../types/ValuesOf';
 
 const MatchOrderBy = {
   kickOff: 'kickOff',
 } as const;
 
-type MatchOrderBy = typeof MatchOrderBy[keyof typeof MatchOrderBy];
+type MatchOrderBy = ValuesOf<typeof MatchOrderBy>;
 
 export class FindMatchesArgs {
   @IsOptional()
